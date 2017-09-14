7a3e8894c9070b930be6e0528b52c54a682c24c8
Jo <josunghyun89@naver.com>
2017-09-14 21:29:54 +0900

    interface

--- src/main/java/com/okdevtv/java/App.java
+++ src/main/java/com/okdevtv/java/App.java
@@ -4,10 +4,15 @@ package com.okdevtv.java;
  * Hello world!
  *
  */
-public class App 
+public class App  implements AppInterface 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
